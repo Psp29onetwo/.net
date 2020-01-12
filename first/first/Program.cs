3974bfd38c8288a195a90c2e5c6fd7d07176eec4
Paurav(#PSP) <pspofficial1999@gmail.com>
2020-01-12 06:25:42 +0530

    Add files via upload

---
+++ first/first/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the value of first number");
+            int number1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the value of second number");
+            int number2 = int.Parse(Console.ReadLine());
+            int temp = number1;
+            number1 = number2;
+            number2 = temp;
+            Console.WriteLine("value of number1 " + number1 + " Value of number2 " + number2);
+
+
+        }
+    }
+}
